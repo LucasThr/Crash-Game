c4b0ab2ab9b1677c03ab4cb76ccfa2e7c778d0d5
Thirion Lucas <lucasthirion@live.fr>
2021-05-02 22:41:28 +0200

    fix: restrict decimal

--- src/app/graphics/graphics.component.ts
+++ src/app/graphics/graphics.component.ts
@@ -9,9 +9,9 @@ import { BetRecord } from '../bet';
   styleUrls: ['../app.component.css'],
 })
 export class GraphicsComponent implements OnInit {
-  money:number=100;
-  mise!:number
-  bets:BetRecord[] = [{initial:10,value:15,gain:200},{initial:10,value:15,gain:200},{initial:10,value:15,gain:200}];
+  money: number = 100;
+  mise!: number;
+  bets: BetRecord[] = [];
   // bet: Bet = { id: 1, value: 0 };
   time!: number;
   timer!: number;
@@ -25,19 +25,25 @@ export class GraphicsComponent implements OnInit {
   start() {
     var crash: number;
     var max: number;
-    this.isWithdraw=false
+    this.isWithdraw = false;
     max = (Math.floor(Math.random() * (500 - 0) + 0) / 100) * 100;
     this.timer = window.setInterval(() => {
-      if (this.time > 0 && this.time < 2){
-          this.time=Number(Number((Math.round((this.time += 0.01) * 100) / 100)).toFixed(2))
-       }
+      if (this.time > 0 && this.time < 2) {
+        this.time = Number(
+          Number(Math.round((this.time += 0.01) * 100) / 100).toFixed(2)
+        );
+      }
       if (this.time < 5 && this.time >= 2) {
-        this.time=Number(Number(this.time += 0.04).toFixed(2))
+        this.time = Number(
+          Number(Math.round((this.time += 0.04) * 100) / 100).toFixed(2)
+        );
 
         max = max / 2;
       }
       if (this.time < 100 && this.time >= 5) {
-        this.time=Number(Number(this.time += 0.08).toFixed(2))
+        this.time = Number(
+          Number(Math.round((this.time += 0.08) * 100) / 100).toFixed(2)
+        );
         max = max / 2;
       }
       crash = (Math.floor(Math.random() * (max - 1) + 1) / 100) * 100;
@@ -45,7 +51,9 @@ export class GraphicsComponent implements OnInit {
       if (crash == 1) {
         clearTimeout(this.timer);
         this.isCrash = true;
-
+        if(!this.isWithdraw){
+        this.addBet(this.mise, this.time,true);
+        }
       }
     }, 100);
   }
@@ -55,30 +63,34 @@ export class GraphicsComponent implements OnInit {
   }
 
   onMise(): void {
-    if(!this.mise) return
-    this.money-=this.mise
-    this.isCrash=false
-    this.time=1.00
+    if (!this.mise) return;
+    if((this.money-this.mise)<0) return;
+    this.money=this.fixedDecimal(this.money- this.mise);
+    this.isCrash = false;
+    this.time = 1.0;
     this.start();
-   
   }
 
-  addMoney(value:number){
-    this.money+=(value*this.mise)
+  addMoney(time: number) {
+    // Number(this.money+= time * this.mise).toFixed(2);
+    this.money= Number((this.money+ time * this.mise).toFixed(2));
+
   }
 
-  addBet(initial:number,value:number,gain:number){
-    this.bets.push({initial:10,value:value,gain:200})
+  addBet(initial: number, value: number,isLose?:boolean) {
+    this.bets.push({ initial: initial, value: value, gain: isLose ? 0 :this.fixedDecimal(initial * value)  });
   }
-
 
   onRetrait(): void {
-    if(this.isWithdraw)return
-    this.isWithdraw=true
-    console.log("RETRAIT")
-    this.addMoney(this.time)
-    this.addBet(10,this.time,10)
+    if (this.isWithdraw) return;
+    this.isWithdraw = true;
+    console.log('RETRAIT');
+    this.addMoney(this.time);
+    this.addBet(this.mise, this.time);
   }
 
+  fixedDecimal(number: number): number {
+    return Number(number.toFixed(2));
+  }
   ngOnInit(): void {}
 }
