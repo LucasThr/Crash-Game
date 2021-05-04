719bf01507a3034300dc8c4803ef154b707ac076
Thirion Lucas <lucasthirion@live.fr>
2021-05-04 17:54:17 +0200

    feat:design chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,40 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { ChatService } from '../chat.service'; 
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ChatService } from '../chat.service';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['../app.component.css'],
 })
 export class ChatComponent implements OnInit {
-  msg = 'First Protocol'
-  newMessage:string="MESSAGE"
-  messageList:  string[] = [];
+  msg = 'First Protocol';
+  newMessage: string = 'MESSAGE';
+  messageList: string[] = [];
   constructor(
-    private chatService:ChatService
-  ) { }
+  //   @ViewChild('chat') private myScrollContainer: ElementRef,
 
-    sendMessage() {
-      console.log(this.newMessage)
-      this.chatService.sendChat(this.newMessage);
-    }
+    private chatService: ChatService
+  ) {}
 
+  // name = 'Angular';
 
-  ngOnInit(): void {
-    this.chatService.chat
-      .subscribe((message) => {
-        console.log('message recu')
-        this.messageList.push(message);
-        this.chatService.getChats(this.messageList)
-      });
-      this.chatService.chats
-      .subscribe((messages) => {
-        console.log('message recu !')
-        this.messageList=messages
-      });
+  // scrollToElement(el:ElementRef): void {
+  //   this.myScrollContainer.nativeElement.scroll({
+  //     top: this.myScrollContainer.nativeElement.scrollHeight,
 
+  //     left: 0,
+
+  //     behavior: 'smooth',
+  //   });
+  // }
+
+  sendMessage() {
+    console.log(this.newMessage);
+    this.chatService.sendChat(this.newMessage);
   }
 
-
-  
-
+  ngOnInit(): void {
+    this.chatService.chat.subscribe((message) => {
+      console.log('message recu');
+      this.messageList.push(message);
+      this.chatService.getChats(this.messageList);
+    });
+    this.chatService.chats.subscribe((messages) => {
+      console.log('message recu !');
+      this.messageList = messages;
+    });
+  }
 }
