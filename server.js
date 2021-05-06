2b79932d2560409f51c077b28e31442113cd97a0
Thirion Lucas <lucasthirion@live.fr>
2021-05-06 11:15:00 +0200

    test deploy heroku 9

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/rocket-money/dist/'));
 
-app.get('*', (req,res) => {
+app.get('/*', (req,res) => {
     res.sendFile(__dirname + '/rocket-money/dist/index.html');
 });
 
