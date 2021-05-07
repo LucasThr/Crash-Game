0a55dace2c29c00582f531e7f1e3d864de589bc6
Thirion Lucas <lucasthirion@live.fr>
2021-05-07 14:29:32 +0200

    remis en mode local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,11 @@ import { BetComponent } from './bet/bet.component';
 import { GraphicsComponent } from './graphics/graphics.component';
 import { ChatComponent } from './chat/chat.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 // const port = 8000;
 console.log(port)
 // const config: SocketIoConfig = { url: 'localhost:8000', options: {} };
-const config: SocketIoConfig = { url: `https://lucasthr-crashgame.herokuapp.com:${port}`, options: {} };
+const config: SocketIoConfig = { url: `localhost:${port}`, options: {} };
 
 @NgModule({
   declarations: [AppComponent, BetComponent, GraphicsComponent, ChatComponent],
